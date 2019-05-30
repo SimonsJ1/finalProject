40d62466321c4154bcf99e1e6743f7465e5e734e
Jake Simons <784108@apps.district196.org>
2019-05-29 20:04:25 -0500

    added buttons for remaining squares and added setting gameData

--- src/methods/Display.java
+++ src/methods/Display.java
@@ -47,135 +47,269 @@ public class Display extends JPanel {
 		JButton buttonAgain = createButton(50, 520, 120, 30, "Play Again?", false);
 	    buttonAgain.addActionListener(new ActionListener() {
 	    	public void actionPerformed(ActionEvent e) {
-	    		// reset all board to empty and all data to empty
+	    		// **** reset all board to empty and all data to empty
+	    		// set all to invisible
 	    		repaint();
 	    	};
 	    });
-	
-	
+		
 	    // Square 1 toggle
 		JButton button1 = createButton(90, 100, 100, 110, "", true);
 	    button1.addActionListener(new ActionListener() {
 	    	public void actionPerformed(ActionEvent e) {
 	    		// check if marked - if so do not change.
-	    		if (gameData.isXTurn()) {
-	    			// COULD REPLACE WITH VISIBLE METHOD CALLS
-	    			// ALSO DECIDE ON COLORS YOU WANT FOR X's and O's
-	    			gamePieces.colorX1 = Color.RED;
-	    			gamePieces.colorO1 = transparent;
-	    			gamePieces.colorInnerO1 = transparent;
-	    		}
-	    		else {
-	    			// COULD REPLACE WITH VISIBLE METHOD CALLS
-	    			gamePieces.colorX1 = transparent;
-	    			gamePieces.colorO1 = Color.RED;
-	    			gamePieces.colorInnerO1 = Color.WHITE;
-	    		}
-	    		gameData.setXTurn();
-	    		label.setText(gameData.getDialog());
-	    		//update data with marked square and check for win or tie
-	    		repaint();
+	    	    if (!gameData.isGameValueSet(0, 0)) {		
+	    	    	if (gameData.isXTurn()) {
+	    	    		// COULD REPLACE WITH VISIBLE METHOD CALLS
+	    	    		// ALSO DECIDE ON COLORS YOU WANT FOR X's and O's
+	    	    		gamePieces.colorX1 = Color.RED;
+	    	    		gamePieces.colorO1 = transparent;
+	    	    		gamePieces.colorInnerO1 = transparent;
+	    	    	}
+	    	    	else {
+	    	    		// COULD REPLACE WITH VISIBLE METHOD CALLS
+	    	    		gamePieces.colorX1 = transparent;
+	    	    		gamePieces.colorO1 = Color.RED;
+	    	    		gamePieces.colorInnerO1 = Color.WHITE;
+	    	    	}
+	    	    	gameData.setGameValue(0,0) ;
+	    	    	gameData.checkGameState();
+	    	    	gameData.setXTurn();			
+	    	    	label.setText(gameData.getDialog());
+	    	    	//update data with marked square and check for win or tie
+	    	    	repaint();
+	    	    };
 	    	};
 	    });
 
-	    
-	    
-	    
-	    
-	    
-	    
-	    
-	    
-	    
-	    
-	    
-	    // FIX and CODE REST OF SQUARES
-	    
-	    JButton button2= new JButton("");
+	    JButton button2 = createButton(90+150, 100, 140, 110, "", true);
 	    button2.addActionListener(new ActionListener() {
 	    	public void actionPerformed(ActionEvent e) {
-	    		gamePieces.colorO5 = Color.GREEN;
-	    		repaint();
+	    		
+	    	    if (!gameData.isGameValueSet(0, 1)) {		
+	    	    	if (gameData.isXTurn()) {
+	    	    		gamePieces.colorX2 = Color.RED;
+	    	    		gamePieces.colorO2 = transparent;
+	    	    		gamePieces.colorInnerO2 = transparent;
+	    	    	}
+	    	    	else {
+	    	    		gamePieces.colorX2 = transparent;
+	    	    		gamePieces.colorO2 = Color.RED;
+	    	    		gamePieces.colorInnerO2 = Color.WHITE;
+	    	    	}
+	    	    	gameData.setGameValue(0,1) ;
+	    	    	gameData.checkGameState();
+	    	    	gameData.setXTurn();			
+	    	    	label.setText(gameData.getDialog());
+	    	    	//update data with marked square and check for win or tie
+	    	    	repaint();
+	    	    };
 	    	};
 	    });
-	    button2.setBounds(90+150, 100, 140, 110);
-	    button2.setContentAreaFilled(false);
-	    add(button2);
 
-	    JButton button3= new JButton("");
+	    // Square 3 toggle
+		JButton button3 = createButton(90+310, 100, 130, 110, "", true);
 	    button3.addActionListener(new ActionListener() {
 	    	public void actionPerformed(ActionEvent e) {
-	    		gamePieces.colorO5 = Color.BLACK;
-	    		repaint();
+	    		// check if marked - if so do not change.
+	    	    if (!gameData.isGameValueSet(0, 2)) {		
+	    	    	if (gameData.isXTurn()) {
+	    	    		// COULD REPLACE WITH VISIBLE METHOD CALLS
+	    	    		// ALSO DECIDE ON COLORS YOU WANT FOR X's and O's
+	    	    		gamePieces.colorX3 = Color.RED;
+	    	    		gamePieces.colorO3 = transparent;
+	    	    		gamePieces.colorInnerO3 = transparent;
+	    	    	}
+	    	    	else {
+	    	    		// COULD REPLACE WITH VISIBLE METHOD CALLS
+	    	    		gamePieces.colorX3 = transparent;
+	    	    		gamePieces.colorO3 = Color.RED;
+	    	    		gamePieces.colorInnerO3 = Color.WHITE;
+	    	    	}
+	    	    	gameData.setGameValue(0,2) ;
+	    	    	gameData.checkGameState();
+	    	    	gameData.setXTurn();			
+	    	    	label.setText(gameData.getDialog());
+	    	    	//update data with marked square and check for win or tie
+	    	    	repaint();
+	    	    };
 	    	};
 	    });
-	    button3.setBounds(90+310, 100, 130, 110);
-	    button3.setContentAreaFilled(false);
-	    add(button3);
-	
-	    JButton button4= new JButton("");
+
+	    // Square 4 toggle
+		JButton button4 = createButton(90, 100+130, 130, 120, "", true);
 	    button4.addActionListener(new ActionListener() {
 	    	public void actionPerformed(ActionEvent e) {
-	    		gamePieces.colorO5 = Color.BLUE;
-	    		repaint();
+	    		// check if marked - if so do not change.
+	    	    if (!gameData.isGameValueSet(1, 0)) {		
+	    	    	if (gameData.isXTurn()) {
+	    	    		// COULD REPLACE WITH VISIBLE METHOD CALLS
+	    	    		// ALSO DECIDE ON COLORS YOU WANT FOR X's and O's
+	    	    		gamePieces.colorX4 = Color.RED;
+	    	    		gamePieces.colorO4 = transparent;
+	    	    		gamePieces.colorInnerO4 = transparent;
+	    	    	}
+	    	    	else {
+	    	    		// COULD REPLACE WITH VISIBLE METHOD CALLS
+	    	    		gamePieces.colorX4 = transparent;
+	    	    		gamePieces.colorO4 = Color.RED;
+	    	    		gamePieces.colorInnerO4 = Color.WHITE;
+	    	    	}
+	    	    	gameData.setGameValue(1,0) ;
+	    	    	gameData.checkGameState();
+	    	    	gameData.setXTurn();			
+	    	    	label.setText(gameData.getDialog());
+	    	    	//update data with marked square and check for win or tie
+	    	    	repaint();
+	    	    };
 	    	};
 	    });
-	    button4.setBounds(90, 100+130, 130, 120);
-	    button4.setContentAreaFilled(false);
-	    add(button4);
 
-	    JButton button7= new JButton("") {
-            @Override public void setBorder(Border border) {
-                // No!
-            }
-        };
+	    // Square 5 toggle
+		JButton button5 = createButton(90+150, 100+130, 140, 120, "", true);
+	    button5.addActionListener(new ActionListener() {
+	    	public void actionPerformed(ActionEvent e) {
+	    		// check if marked - if so do not change.
+	    	    if (!gameData.isGameValueSet(1, 1)) {		
+	    	    	if (gameData.isXTurn()) {
+	    	    		// COULD REPLACE WITH VISIBLE METHOD CALLS
+	    	    		// ALSO DECIDE ON COLORS YOU WANT FOR X's and O's
+	    	    		gamePieces.colorX5 = Color.RED;
+	    	    		gamePieces.colorO5 = transparent;
+	    	    		gamePieces.colorInnerO5 = transparent;
+	    	    	}
+	    	    	else {
+	    	    		// COULD REPLACE WITH VISIBLE METHOD CALLS
+	    	    		gamePieces.colorX5 = transparent;
+	    	    		gamePieces.colorO5 = Color.RED;
+	    	    		gamePieces.colorInnerO5 = Color.WHITE;
+	    	    	}
+	    	    	gameData.setGameValue(1,1) ;
+	    	    	gameData.checkGameState();
+	    	    	gameData.setXTurn();			
+	    	    	label.setText(gameData.getDialog());
+	    	    	//update data with marked square and check for win or tie
+	    	    	repaint();
+	    	    };
+	    	};
+	    });
+
+	    // Square 6 toggle
+		JButton button6 = createButton(90+310, 100+130, 130, 120, "", true);
+	    button6.addActionListener(new ActionListener() {
+	    	public void actionPerformed(ActionEvent e) {
+	    		// check if marked - if so do not change.
+	    	    if (!gameData.isGameValueSet(1, 2)) {		
+	    	    	if (gameData.isXTurn()) {
+	    	    		// COULD REPLACE WITH VISIBLE METHOD CALLS
+	    	    		// ALSO DECIDE ON COLORS YOU WANT FOR X's and O's
+	    	    		gamePieces.colorX6 = Color.RED;
+	    	    		gamePieces.colorO6 = transparent;
+	    	    		gamePieces.colorInnerO6 = transparent;
+	    	    	}
+	    	    	else {
+	    	    		// COULD REPLACE WITH VISIBLE METHOD CALLS
+	    	    		gamePieces.colorX6 = transparent;
+	    	    		gamePieces.colorO6 = Color.RED;
+	    	    		gamePieces.colorInnerO6 = Color.WHITE;
+	    	    	}
+	    	    	gameData.setGameValue(1,2) ;
+	    	    	gameData.checkGameState();
+	    	    	gameData.setXTurn();			
+	    	    	label.setText(gameData.getDialog());
+	    	    	//update data with marked square and check for win or tie
+	    	    	repaint();
+	    	    };
+	    	};
+	    });
+
+	    // Square 7 toggle
+		JButton button7 = createButton(90, 100+270, 130, 130, "", true);
 	    button7.addActionListener(new ActionListener() {
 	    	public void actionPerformed(ActionEvent e) {
-	    		if (gameData.isXTurn()) {
-	    			gamePieces.colorX7 = Color.CYAN;
-	    			gamePieces.colorO7 = transparent;
-	    			gamePieces.colorInnerO7 = transparent;
-	    		}
-	    		else {
-	    			gamePieces.colorX7 = transparent;
-	    			gamePieces.colorO7 = Color.CYAN;
-	    			gamePieces.colorInnerO7 = Color.WHITE;
-	    		}
-	    		gameData.setXTurn();
-	    		label.setText(gameData.getDialog());
-	    		//update data with marked square and check for win or tie
-	    		repaint();
+	    		// check if marked - if so do not change.
+	    	    if (!gameData.isGameValueSet(2, 0)) {		
+	    	    	if (gameData.isXTurn()) {
+	    	    		// COULD REPLACE WITH VISIBLE METHOD CALLS
+	    	    		// ALSO DECIDE ON COLORS YOU WANT FOR X's and O's
+	    	    		gamePieces.colorX7 = Color.RED;
+	    	    		gamePieces.colorO7 = transparent;
+	    	    		gamePieces.colorInnerO7 = transparent;
+	    	    	}
+	    	    	else {
+	    	    		// COULD REPLACE WITH VISIBLE METHOD CALLS
+	    	    		gamePieces.colorX7 = transparent;
+	    	    		gamePieces.colorO7 = Color.RED;
+	    	    		gamePieces.colorInnerO7 = Color.WHITE;
+	    	    	}
+	    	    	gameData.setGameValue(2,0) ;
+	    	    	gameData.checkGameState();
+	    	    	gameData.setXTurn();			
+	    	    	label.setText(gameData.getDialog());
+	    	    	//update data with marked square and check for win or tie
+	    	    	repaint();
+	    	    };
 	    	};
 	    });
-	    button7.setBounds(90, 100+270, 130, 130);
-	    button7.setContentAreaFilled(false);
-	    add(button7);
 
-	
-	    
-	    
-	    
-	    
-	    
-	    
-	    
-	    
-	    
-	    
-	    
-	    
-	    
-	    
-	    
-	    
-	    
-	    
-	    
-	    
-	    
-	    
-	    
-	    
+	    // Square 8 toggle
+		JButton button8 = createButton(90+150, 100+270, 140, 130, "", true);
+	    button8.addActionListener(new ActionListener() {
+	    	public void actionPerformed(ActionEvent e) {
+	    		// check if marked - if so do not change.
+	    	    if (!gameData.isGameValueSet(2, 1)) {		
+	    	    	if (gameData.isXTurn()) {
+	    	    		// COULD REPLACE WITH VISIBLE METHOD CALLS
+	    	    		// ALSO DECIDE ON COLORS YOU WANT FOR X's and O's
+	    	    		gamePieces.colorX8 = Color.RED;
+	    	    		gamePieces.colorO8 = transparent;
+	    	    		gamePieces.colorInnerO8 = transparent;
+	    	    	}
+	    	    	else {
+	    	    		// COULD REPLACE WITH VISIBLE METHOD CALLS
+	    	    		gamePieces.colorX8 = transparent;
+	    	    		gamePieces.colorO8 = Color.RED;
+	    	    		gamePieces.colorInnerO8 = Color.WHITE;
+	    	    	}
+	    	    	gameData.setGameValue(2,1) ;
+	    	    	gameData.checkGameState();
+	    	    	gameData.setXTurn();			
+	    	    	label.setText(gameData.getDialog());
+	    	    	//update data with marked square and check for win or tie
+	    	    	repaint();
+	    	    };
+	    	};
+	    });
+
+	    // Square 9 toggle
+		JButton button9 = createButton(90+310, 100+270, 130, 130, "", true);
+	    button9.addActionListener(new ActionListener() {
+	    	public void actionPerformed(ActionEvent e) {
+	    		// check if marked - if so do not change.
+	    	    if (!gameData.isGameValueSet(2, 2)) {		
+	    	    	if (gameData.isXTurn()) {
+	    	    		// COULD REPLACE WITH VISIBLE METHOD CALLS
+	    	    		// ALSO DECIDE ON COLORS YOU WANT FOR X's and O's
+	    	    		gamePieces.colorX1 = Color.RED;
+	    	    		gamePieces.colorO1 = transparent;
+	    	    		gamePieces.colorInnerO9 = transparent;
+	    	    	}
+	    	    	else {
+	    	    		// COULD REPLACE WITH VISIBLE METHOD CALLS
+	    	    		gamePieces.colorX9 = transparent;
+	    	    		gamePieces.colorO9 = Color.RED;
+	    	    		gamePieces.colorInnerO9 = Color.WHITE;
+	    	    	}
+	    	    	gameData.setGameValue(2,2) ;
+	    	    	gameData.checkGameState();
+	    	    	gameData.setXTurn();			
+	    	    	label.setText(gameData.getDialog());
+	    	    	//update data with marked square and check for win or tie
+	    	    	repaint();
+	    	    };
+	    	};
+	    });
+
         label = new JTextField()
         {
             @Override public void setBorder(Border border) {
--- src/methods/GameData.java
+++ src/methods/GameData.java
@@ -11,13 +11,23 @@ public class GameData {
 	boolean xTurn = true;
     String p1Turn = " Player 1's turn";
     String p2Turn = " Player 2's turn";
-    // print to screen "Player 1 WON!"
-    // print to screen "Player 2 WON!"
-    // print to screen "Tie game, play again?"
+	String p1Won = " Player 1 WON!";
+	String p2Won = " Player 2 WON!";
+	String tie = " Tie game, play again?";
+	int[][] gameValues = new int[3][3];
+	int xcount = 0;
+	int ocount = 0;
+	int winner = 0;
 	
 	public GameData()
 	{
+	    for(int k=0; k<3; k++){
+	    	for(int j=0; j<3; j++){  //checks for a winner
+	    		gameValues[k][j] = 0;
+	    	}
+	    }
 	}
+	
 	
 	public boolean isXTurn() {
 		return xTurn;
@@ -33,12 +43,73 @@ public class GameData {
 	}
 
 	public String getDialog() {
-		// ADD for WIN and TIE messages
-		if (isXTurn()) {
+		if (winner > 0 && winner == 1) {
+			return p1Won;
+		}
+		else if (winner > 0 && winner == 2) {
+			return p2Won;
+		}
+		if (winner > 0 && winner == 3) {
+			return tie;
+		}
+		else if (isXTurn()) {
 			return p1Turn;
 		}
 		else {
 			return p2Turn;
 		}
+	}
+	
+	public void setGameValue(int row, int col) {
+		if (isXTurn())
+			gameValues[row][col] = 1;
+		else
+			gameValues[row][col] = 2;
+	}
+
+	public boolean isGameValueSet(int row, int col) {
+		if (gameValues[row][col] > 0) {
+			return true;
+		}
+		else if (winner > 0) {
+			return true;
+		}
+		return false;
+	}
+	
+	public boolean checkGameState() {
+		xcount = 0;
+		ocount = 0;
+		winner = 0;
+		
+	    for(int k=0; k<3; k++){
+	       for(int j=0; j<3; j++){  //checks for a winner
+	          if (gameValues[k][j] == 1)
+	             xcount++;
+	          else if (gameValues[k][j] == 2)
+	             ocount++;
+	       }
+	       
+	       if(xcount == 3) {
+	    	   // if 3 1 or X acrosss WIN
+	    	   winner = 1;
+	    	   return true;
+	       }
+    	   else if(ocount ==3) {
+    		   // if 3 2 or O acrosss WIN
+    		   winner = 2;
+    		   return true;
+    	   }
+    	   else {
+    		   // else reset and check next row
+    		   xcount = 0;
+    		   ocount = 0;
+    	   }
+	    }
+
+       // *****need to repeat everything 2 once checking all vertical then diagonial;
+	   // *****check for tie game. 
+
+	    return false;
 	}
 }
